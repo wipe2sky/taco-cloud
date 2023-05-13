9ed766361c1e356b66f984041d1ce7198b152413
Sergey Kurtsevich (Ext) <sergey.kurtsevich@tietoevry.com>
2023-05-13 23:33:02 +0300

    added order list representation

--- src/main/java/com/sia/tacos/controller/OrderController.java
+++ src/main/java/com/sia/tacos/controller/OrderController.java
@@ -2,11 +2,14 @@ package com.sia.tacos.controller;
 
 import com.sia.tacos.entity.TacoOrder;
 import com.sia.tacos.entity.User;
+import com.sia.tacos.properties.OrderProps;
 import com.sia.tacos.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -17,14 +20,22 @@ import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
 
-@Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("order")
 @RequiredArgsConstructor
 public class OrderController {
 
+    private final OrderProps props;
     private final OrderRepository orderRepo;
+
+    @GetMapping
+    public String ordersForUser(
+            @AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, props.getPageSize());
+        model.addAttribute("orders", orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+        return "orderList";
+    }
 
     @GetMapping("/current")
     public String orderForm(@AuthenticationPrincipal User user,
--- src/main/java/com/sia/tacos/repository/OrderRepository.java
+++ src/main/java/com/sia/tacos/repository/OrderRepository.java
@@ -1,7 +1,13 @@
 package com.sia.tacos.repository;
 
 import com.sia.tacos.entity.TacoOrder;
+import com.sia.tacos.entity.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
+
 public interface OrderRepository extends CrudRepository<TacoOrder, String> {
+    List<TacoOrder> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
