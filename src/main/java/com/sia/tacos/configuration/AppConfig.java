9a17a46a2e87a2c8043b983b3dd1f2fd565a6a0e
Sergey Kurtsevich (Ext) <sergey.kurtsevich@tietoevry.com>
2023-05-14 15:57:36 +0300

    added profiles

--- src/main/java/com/sia/tacos/configuration/AppConfig.java
+++ src/main/java/com/sia/tacos/configuration/AppConfig.java
@@ -8,11 +8,13 @@ import com.sia.tacos.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
     @Bean
+    @Profile("dev")
     public CommandLineRunner dataLoader(IngredientRepository repository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
         return args -> {
             repository.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
