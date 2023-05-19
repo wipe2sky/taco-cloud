2093330756ef99e769f3c16aa20c2c378a350d2c
Sergey Kurtsevich (Ext) <sergey.kurtsevich@tietoevry.com>
2023-05-19 16:11:01 +0300

    some refactoring sping security

--- src/main/java/com/kurtsevich/tacos/configuration/SecurityConfig.java
+++ src/main/java/com/kurtsevich/tacos/configuration/SecurityConfig.java
@@ -1,12 +1,9 @@
 package com.kurtsevich.tacos.configuration;
 
-import com.kurtsevich.tacos.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,11 +17,6 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    UserDetailsService userDetailsService(UserRepository userRepo) {
-        return username -> userRepo.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User '" + username + "' not found"));
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -45,7 +37,6 @@ public class SecurityConfig {
                 .logout()
                 .logoutSuccessUrl("/")
 
-                // Make H2-Console non-secured; for debug purposes
                 .and()
                 .csrf()
                 .ignoringAntMatchers("/h2-console/**")
---
+++ src/main/java/com/kurtsevich/tacos/service/UserRepositoryUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.kurtsevich.tacos.service;
+
+import com.kurtsevich.tacos.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserRepositoryUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User '" + username + "' not found"));
+    }
+}
