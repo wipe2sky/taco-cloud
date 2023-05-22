7ebc2fef37d23642a62ee70a7a750da047014a5e
Sergey Kurtsevich (Ext) <sergey.kurtsevich@tietoevry.com>
2023-05-22 21:51:23 +0300

    created reactive endpoints

---
+++ src/main/java/com/kurtsevich/tacos/controller/TacoController.java
@@ -0,0 +1,41 @@
+package com.kurtsevich.tacos.controller;
+
+import com.kurtsevich.tacos.entity.Taco;
+import com.kurtsevich.tacos.repository.TacoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping(path = "/api/tacos",
+        produces = "application/json")
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+public class TacoController {
+    private final TacoRepository tacoRepo;
+
+    @GetMapping("/{id}")
+    public Mono<Taco> tacoById(@PathVariable("id") Long id) {
+        return tacoRepo.findById(id);
+    }
+
+    @GetMapping(params = "recent")
+    public Flux<Taco> getRecentTacos() {
+        return tacoRepo.findAll().take(12);
+    }
+
+    @PostMapping(consumes="application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Taco> postTaco(@RequestBody Mono<Taco> tacoMono) {
+        return tacoRepo.saveAll(tacoMono).next();
+    }
+}
--- src/main/java/com/kurtsevich/tacos/repository/TacoRepository.java
+++ src/main/java/com/kurtsevich/tacos/repository/TacoRepository.java
@@ -1,7 +1,7 @@
 package com.kurtsevich.tacos.repository;
 
 import com.kurtsevich.tacos.entity.Taco;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
-public interface TacoRepository extends PagingAndSortingRepository<Taco, Long> {
+public interface TacoRepository extends ReactiveCrudRepository<Taco, Long> {
 }
